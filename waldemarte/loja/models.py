0f3824e2b6e271c180d9adafd196ea1eb7e3b924
Eduarda <eduardatuboy@gmail.com>
2024-12-02 09:09:24 -0300

    configuracoes iniciais do projeto

---
+++ waldemarte/loja/models.py
@@ -0,0 +1,3 @@
+from django.db import models
+
+# Create your models here. -> informacoes que vamos armazenar no BD
---
+++ waldemarte/loja/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render
+
+# Create your views here. -> interacao que o usuario tem no frontend vem pra ca
