0873a86517712f79b6e7947ecb0be6e5cabc0ae9
FelipeCarneiroMachado <felipecmachado05@gmail.com>
2024-12-02 10:21:54 -0300

    Opcao, Usuario, Produto criados no model

--- waldemarte/loja/models.py
+++ waldemarte/loja/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here. -> informacoes que vamos armazenar no BD
+
+
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    preco = models.FloatField()
+    # TODO : avaliacoes, requer classe avaliacao
+    # opcoes : implementado como foreign key na classe Opcao
+    especificacoes = models.TextField()
+    estoque = models.IntegerField()
+
+
+
+# Poduto contem uma lista de opcoes, o melhor jeito de implementa-la eh com uma table separada (One to Many)
+class Opcao(models.Model):
+    opcao = models.CharField(max_length=255)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+
+
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length=20)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=20)
+    cpf = models.CharField(max_length=15)
+    # TODO: Lista de transacoes, requer classe transacao primeiro
+
+
+
